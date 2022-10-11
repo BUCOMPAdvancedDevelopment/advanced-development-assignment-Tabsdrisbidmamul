f15b68c3444540ede130bed8d50e5aa496049e03
Idris Khan <idris.khan303@gmail.com>
2022-10-11 08:03:30 +0100

    Fix url strucutre for dev and prod builds for angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,43 +10,29 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<DataContext>(opt => 
 {
   opt
     .UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-if(builder.Environment.IsDevelopment()) 
+
+builder.Services.AddCors(opt => 
 {
-  builder.Services.AddCors(opt => 
+  opt.AddPolicy("AllowAll", policy =>
   {
-    opt.AddPolicy("AllowAll", policy =>
-    {
-      policy
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowAnyOrigin()
-        .AllowCredentials();
-    });
+    policy
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+      .AllowAnyOrigin();
   });
-}
+});
 
 
 builder.Services.AddMediatR(typeof(List.Handler).Assembly);
 
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 // app.UseHttpsRedirection();
 
--- client-app/src/app/services/http/http-client.service.ts
+++ client-app/src/app/services/http/http-client.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, isDevMode } from '@angular/core';
-import { catchError } from 'rxjs';
+import { catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,17 +8,17 @@ import { catchError } from 'rxjs';
 export class HttpClientService {
   private domain = isDevMode()
     ? 'https://localhost:5000/'
-    : window.location.origin;
+    : `${window.location.origin}/`;
 
   constructor(private _httpClient: HttpClient) {}
 
   getAllGames() {
-    console.log('isDevMode ', isDevMode());
+    console.log('`${this.domain}api/games` ', `${this.domain}api/games`);
 
-    return this._httpClient.get(`${this.domain}/api/games`).pipe(
+    return this._httpClient.get(`${this.domain}api/games`).pipe(
       catchError((e) => {
-        console.log(e);
-        return e;
+        console.error(e);
+        return of(e);
       })
     );
   }
