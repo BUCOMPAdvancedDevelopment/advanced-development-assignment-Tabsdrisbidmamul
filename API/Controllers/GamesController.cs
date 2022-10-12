90556e1b13f4d03c5de8c8bca91170b789f6db0f
Tabsdrisbidmamul <idris.khan303@gmail.com>
2022-10-12 11:53:25 +0100

    test trigger with permissions

--- API/Controllers/GamesController.cs
+++ API/Controllers/GamesController.cs
@@ -25,6 +25,7 @@ namespace API.Controllers
       return StatusCode(StatusCodes.Status201Created, await Mediator
         .Send(new Application.Games.Create.Command {Game = game}));
     }
+
     
   }
 }
