18b430e0018a5c881b54c30544afa15143ef6234
Idris Khan <idris.khan303@gmail.com>
2022-12-02 12:55:14 +0000

    Side menu built

--- client-app/src/app/views/dashboard/dashboard.component.ts
+++ client-app/src/app/views/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(public viewContainerRef: ViewContainerRef) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  handleProfileClick() {}
 
+  handleGamesClick() {}
 }
