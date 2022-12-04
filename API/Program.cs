cd497b2fb7e295cdeb887f4a487e663c153a6c29
Idris Khan <idris.khan303@gmail.com>
2022-12-04 15:08:48 +0000

    Add static file compression

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,11 @@
+using System.IO.Compression;
 using API.Extensions;
 using API.Middleware;
 using Application.Games;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -14,12 +16,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
+builder.Services.AddResponseCompression(options =>
+{
+  options.Providers.Add<GzipCompressionProvider>();
+  options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { 
+  "image/jpeg", "image/png", "application/javascript", "application/font-woff2", "image/svg+xml"});
+  options.EnableForHttps = true;
+});
+
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+  options.Level = CompressionLevel.Optimal;
+});
+
 var app = builder.Build();
 
 // app.UseHttpsRedirection();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseResponseCompression();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
