f11438bf7f8478030d03db007401b997489fb348
Idris Khan <idris.khan303@gmail.com>
2022-12-04 13:15:09 +0000

    Run build of current angular frontend

---
+++ API/wwwroot/common.02865bbc6dfa3c56.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkclient_app=self.webpackChunkclient_app||[]).push([[592],{1973:(l,e,i)=>{i.d(e,{Mv:()=>o,yW:()=>c,zL:()=>n});class c{constructor(s,a){this.oldPassword=s,this.newPassword=a}}class o{constructor(s,a){this.email=s,this.password=a}}class n{constructor(s,a,r,h){this.displayName=s,this.email=a,this.password=r,this.username=h}}}}]);
